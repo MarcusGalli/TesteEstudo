78ac6eca95001c0b60622ba41db3548e6d99df11
MarcusGalli <marcus.galli@gmail.com>
2016-02-02 15:57:05 -0200

    Branch 1.0.0
    Criação da solução com um console para leitura de response

---
+++ LojaApiClient/LojaApiClient/LojaApiClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conteudo;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("");
+            request.Method = "GET";
+
+            WebResponse response = request.GetResponse();
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                conteudo = reader.ReadToEnd();
+            }
+
+            Console.Write(conteudo);
+            Console.Read();
+
+        }
+    }
+}
